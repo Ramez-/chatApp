e2f84b7ad6406e786e9c6f726c2b27023f7e121c
Ramez Elbaroudy <ramezelbaroudy@Ramezs-MacBook-Air.local>
2017-05-09 15:52:10 +0300

    adding react part

--- src/App.js
+++ src/App.js
@@ -1,32 +1,89 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import socket from './socket'
+import Messages from './components/Messages'
+
 import * as firebase from 'firebase';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      speed: 10
+      user: null,
+      message: ""
     };
   }
-  componentDidMount(){
-    console.log("hello");
-    const rootRef = firebase.database().ref();
-    const speedRef = rootRef.child('speed');
-    speedRef.on('value', snap => {
-      this.setState({
-        speed: snap.val()
-      });
-    });
+
+   
+
+  loginWithFacebook() {  
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      var token = result.credential.accessToken;
+      this.setState({user: result.user});
+    }.bind(this));
   }
 
+  logOut() {
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signOut().then(function() {
+      this.setState({user: null});
+    }.bind(this));
+  }
+  
+
+  //   const rootRef = firebase.database().ref();
+  //   const speedRef = rootRef.child('speed');
+  //   speedRef.on('value', snap => {
+  //     this.setState({
+  //       speed: snap.val(),
+  //       user: null
+  //     });
+  //   });
+  // }
+  // componentDidMount(){
+  //   const rootRef = firebase.database().ref('message');
+  //   //const messageRef = rootRef.child('one');
+  //   rootRef.on('value', function(snap) {
+  //     snap.forEach(function(data){
+  //       //console.log(data.val().one);
+  //       this.setState({
+  //       message: data.val().one
+  //     });
+  //     })
+  //   });
+  // }
+    
   render() {
+    const user = this.state.user;
+
     return (
       <div className="App">
-        <h1>{this.state.speed}</h1>
+                   <button onClick={this.loginWithFacebook.bind(this)}>Login with Facebook</button><button onClick={this.logOut.bind(this)}>Log Out</button>
+
+     <Messages />
+
+        <form onSubmit={this.sendMessage}>
+          <input ref={(input)=> this.input = input } autoComplete="off" />
+          <button onClick={this.sendMessage} >Send</button>
+        </form>
+        <h1>{this.state.message}</h1>
       </div>
     );
+  }
+  sendMessage = (event) => {
+    const rootRef = firebase.database().ref('message');
+    
+    rootRef.push().set({
+      one: this.input.value
+
+    })
+    // this.setState({message: this.input.value});
+    event.preventDefault();
+    console.log(this.input.value);
+    socket.emit('chat message', this.input.value);
+    this.input.value = ''
   }
 }
 
---
+++ src/components/Messages/index.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react'
+import socket from './socket'
+import * as firebase from 'firebase';
+
+
+class Messages extends Component {
+  render() {
+    return (<ul>
+      {this.state.messages.map((message, index)=><li key={index} >{message}</li>)}
+    </ul>)
+  }
+  constructor() {
+    super()
+    this.state = {
+      messages: []
+    }
+  }
+  componentWillMount() {
+    socket.on('chat message', (msg) => {
+      this.setState(prevState=>{
+        return {
+          messages: prevState.messages.concat(msg)
+        }
+      })
+    });
+  }
+
+   componentDidMount(){
+
+    const rootRef = firebase.database().ref('message');
+    //const messageRef = rootRef.child('one');
+    rootRef.on('value', (snap) => {
+      snap.forEach((data) => {
+          if(!this.state.messages.includes(data.val().one)){
+          this.state.messages.push(data.val().one)
+          }
+
+        this.setState({
+        messages: this.state.messages
+      });
+
+      })
+    });
+  }
+}
+
+
+export default Messages
